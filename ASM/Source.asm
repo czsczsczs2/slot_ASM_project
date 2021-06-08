0d5ac82a85924e9bd27d9c1302ea022b969efa7f
A1085520 <a1085520@mail.nuk.edu.tw>
2021-06-08 10:25:47 +0800

    加入專案檔案。

---
+++ ASM/Source.asm
@@ -0,0 +1,375 @@
+INCLUDE Irvine32.inc
+
+.data
+	watermelon1 BYTE "      :i::::.      ", 0
+	watermelon2 BYTE "   .XDjvrrrjKbL    ", 0
+	watermelon3 BYTE "  :BJ::iriiii:uR.  ", 0
+	watermelon4 BYTE "  BL:rriirir:UKIR  ", 0
+	watermelon5 BYTE "  BriiiiiiriirisQ  ", 0
+	watermelon6 BYTE "  rBri:i:iiiiivB.  ", 0
+	watermelon7 BYTE "   :XSKUUjLL5dP.   ", 0
+	watermelon8 BYTE "      :Y5XLr:.     ", 0
+	watermelon9 BYTE "-------------------", 0
+
+	bell1 BYTE "      id7LX.       ", 0
+	bell2 BYTE "     7Zr::7Z1      ", 0
+	bell3 BYTE "    .d:     Q:     ", 0
+	bell4 BYTE "    Z::     .Q     ", 0
+	bell5 BYTE "  :D:....:.. :Mi   ", 0
+	bell6 BYTE " .Q..irr::rii..Bi  ", 0
+	bell7 BYTE "  d1QBBB: QBBQsg.  ", 0
+	bell8 BYTE "   ....iv7Y.:..    ", 0
+	bell9 BYTE "-------------------", 0
+
+	WTF1 BYTE "    .B. .BB. .B.   ", 0
+    WTF2 BYTE "    BBB.BBBB.BBB   ", 0
+    WTF3 BYTE "   BBBBBMBBBBBBBB  ", 0
+    WTF4 BYTE "  BQBBBBBBQBQBBiU  ", 0
+    WTF5 BYTE "   .BBBMQgRRBQBU   ", 0
+    WTF6 BYTE "     IBBQQBBBrU    ", 0
+    WTF7 BYTE "       dBBBuU      ", 0
+    WTF8 BYTE "        OOO        ", 0
+	WTF9 BYTE "-------------------", 0
+
+	banana1 BYTE "        r:         ", 0
+	banana2 BYTE "         MEi       ", 0
+	banana3 BYTE "          QBBB     ", 0
+	banana4 BYTE "           7BQBv   ", 0
+	banana5 BYTE "          5BgBq    ", 0
+	banana6 BYTE "         5BQQB1    ", 0
+	banana7 BYTE "      vUBRBBB      ", 0
+	banana8 BYTE "   i5qIJJr.        ", 0
+	banana9 BYTE "-------------------", 0
+
+	apple1 BYTE "        dKPDdQr    ", 0
+	apple2 BYTE "       DAMN        ", 0
+	apple3 BYTE "  iBQEDdqqIUSSBr   ", 0
+	apple4 BYTE "  BBUI2I1225P IB   ", 0
+	apple5 BYTE "  iBXUIUI2UUq.B7   ", 0
+	apple6 BYTE "  iBXUIUI2UUq.B7   ", 0
+	apple7 BYTE "   2BDqPDD1IKBQ    ", 0
+	apple8 BYTE "    rRMds5MZQX     ", 0
+	apple9 BYTE "-------------------", 0
+
+	wohoo1 BYTE "                   ", 0
+	wohoo2 BYTE "      .Bb::5.      ", 0
+	wohoo3 BYTE "      .Bg::q.      ", 0
+	wohoo4 BYTE "       VBqBV       ", 0
+	wohoo5 BYTE "        VpV        ", 0
+	wohoo6 BYTE "                   ", 0
+	wohoo7 BYTE "         pp        ", 0
+	wohoo8 BYTE "                   ", 0
+	wohoo9 BYTE "-------------------", 0
+
+	huh1 BYTE "       BBB5D       ", 0
+	huh2 BYTE "     .qv   BQi     ", 0
+	huh3 BYTE "     v     uBQ     ", 0
+	huh4 BYTE "          uBX      ", 0
+	huh5 BYTE "         UYB       ", 0
+	huh6 BYTE "        UU         ", 0
+	huh7 BYTE "                   ", 0
+	huh8 BYTE "        7i         ", 0
+	huh9 BYTE "-------------------", 0
+
+	seven1 BYTE "                   ", 0
+	seven2 BYTE " JBBBBQQ BBBBBMBr  ", 0
+	seven3 BYTE " 7Bi QDM EBi  BgR  ", 0
+	seven4 BYTE "    2PPI    Z2R7   ", 0
+	seven5 BYTE "  .Bd2XD  :BK2bd   ", 0
+	seven6 BYTE "  dB525BE BMS2SB:  ", 0
+	seven7 BYTE "  :BBQQB: 7BBBBM   ", 0
+	seven8 BYTE "    ...     ...    ", 0
+	seven9 BYTE "-------------------", 0
+
+	star1 BYTE "                   ", 0
+    star2 BYTE "         7         ", 0
+    star3 BYTE "       7777        ", 0
+    star4 BYTE "   777777777777    ", 0
+    star5 BYTE "     77777777      ", 0
+    star6 BYTE "     77777777      ", 0
+    star7 BYTE "    777    777     ", 0
+    star8 BYTE "                   ", 0
+	star9 BYTE "-------------------", 0
+
+	watermelon DWORD OFFSET watermelon1, OFFSET watermelon2, OFFSET watermelon3, OFFSET watermelon4, OFFSET watermelon5, OFFSET watermelon6, OFFSET watermelon7, OFFSET watermelon8, OFFSET watermelon9
+	bell	   DWORD OFFSET bell1, OFFSET bell2, OFFSET bell3, OFFSET bell4, OFFSET bell5, OFFSET bell6, OFFSET bell7, OFFSET bell8, OFFSET bell9
+	WTF		   DWORD OFFSET WTF1, OFFSET WTF2, OFFSET WTF3, OFFSET WTF4, OFFSET WTF5, OFFSET WTF6, OFFSET WTF7, OFFSET WTF8, OFFSET WTF9
+	banana	   DWORD OFFSET banana1, OFFSET banana2, OFFSET banana3, OFFSET banana4, OFFSET banana5, OFFSET banana6, OFFSET banana7, OFFSET banana8, OFFSET banana9
+	apple	   DWORD OFFSET apple1,OFFSET apple2, OFFSET apple3, OFFSET apple4, OFFSET apple5, OFFSET apple6, OFFSET apple7, OFFSET apple8, OFFSET apple9
+	wohoo	   DWORD OFFSET wohoo1, OFFSET wohoo2, OFFSET wohoo3, OFFSET wohoo4, OFFSET wohoo5, OFFSET wohoo6, OFFSET wohoo7,OFFSET wohoo8, OFFSET wohoo9
+	huh		   DWORD OFFSET huh1, OFFSET huh2, OFFSET huh3, OFFSET huh4, OFFSET huh5, OFFSET huh6, OFFSET huh7, OFFSET huh8, OFFSET huh9
+	seven	   DWORD OFFSET seven1, OFFSET seven2, OFFSET seven3, OFFSET seven4, OFFSET seven5, OFFSET seven6, OFFSET seven7, OFFSET seven8, OFFSET seven9
+	star	   DWORD OFFSET star1, OFFSET star2, OFFSET star3, OFFSET star4, OFFSET star5, OFFSET star6, OFFSET star7, OFFSET star8, OFFSET star9
+
+	count DWORD 0
+	random_num DWORD 0; 總共跑幾次
+	now_block DWORD 0	
+	time_interval DWORD 0
+.code ;
+
+;-----------------------------------------
+Interface PROC
+;-----------------------------------------
+	push ecx
+
+;第一排水果區
+
+	call horizon_edge
+
+	mov count, 0
+    mov ecx, 9
+line1:
+	call vertical_edge
+
+    mov esi, OFFSET watermelon
+	mov eax, 0
+	call paint_block
+
+    mov esi, OFFSET bell
+	mov eax, 1
+	call paint_block
+
+    mov esi, OFFSET WTF
+	mov eax, 2
+	call paint_block
+
+    mov esi, OFFSET banana
+	mov eax, 3
+	call paint_block
+
+    mov esi, OFFSET apple
+	mov eax, 4
+	call paint_block
+
+    mov esi, OFFSET banana
+	mov eax, 5
+	call paint_block
+
+    mov esi, OFFSET watermelon
+	mov eax, 6
+	call paint_block
+
+    call Crlf
+    add count, 4
+	dec ecx
+	cmp ecx, 0
+    jne line1
+
+next_line2:
+;第二排水果+空格
+	mov count, 0
+	mov ecx, 9
+line2:
+	call vertical_edge
+	mov esi, OFFSET watermelon
+	mov eax, 19
+	call paint_block
+	push ecx
+	mov ecx,99
+space1:	
+    mov al, " "
+    call WriteChar
+    loop space1
+	pop ecx
+
+	call vertical_edge
+	mov esi, OFFSET apple
+	mov eax, 7
+	call paint_block
+
+	call Crlf
+	add count, 4
+	loop line2
+
+;第三排水果+空格
+	mov count, 0
+	mov ecx, 9
+line3:
+	call vertical_edge
+	mov esi, OFFSET star
+	mov eax, 18
+	call paint_block
+	push ecx
+	mov ecx,99
+space2:	
+    mov al, " "
+    call WriteChar
+    loop space2
+	pop ecx
+
+	call vertical_edge
+	mov esi, OFFSET apple
+	mov eax, 8
+	call paint_block
+
+	call Crlf
+	add count, 4
+	loop line3
+
+;第四排水果+空格
+
+	mov count, 0
+	mov ecx, 8
+line4:
+	call vertical_edge
+	mov esi, OFFSET star
+	mov eax, 17
+	call paint_block
+	push ecx
+	mov ecx,99
+space3:	
+    mov al, " "
+    call WriteChar
+    loop space3
+	pop ecx
+
+	call vertical_edge
+	mov esi, OFFSET apple
+	mov eax, 9
+	call paint_block
+
+	call Crlf
+	add count, 4
+	loop line4
+	
+	call horizon_edge
+;第五行水果
+	mov count, 0
+	mov ecx, 9
+line5:
+	call vertical_edge
+    mov esi, OFFSET watermelon
+	mov eax, 16
+	call paint_block
+
+    mov esi, OFFSET WTF
+	mov eax, 15
+	call paint_block
+
+    mov esi, OFFSET apple
+	mov eax, 14
+	call paint_block
+
+    mov esi, OFFSET seven
+	mov eax, 13
+	call paint_block
+
+    mov esi, OFFSET banana
+	mov eax, 12
+	call paint_block
+
+    mov esi, OFFSET bell
+	mov eax, 11
+	call paint_block
+
+    mov esi, OFFSET watermelon
+	mov eax, 10
+	call paint_block
+
+    call Crlf
+    add count, 4
+    loop line5
+
+	pop ecx
+    ret
+Interface ENDP
+
+;-----------------------------------------
+vertical_edge PROC
+;-----------------------------------------
+	push eax
+	mov al, "|"
+	call WriteChar
+	pop eax
+	ret
+vertical_edge ENDP
+
+;-----------------------------------------
+horizon_edge PROC
+;-----------------------------------------
+	push eax
+	mov ecx, 141
+Hor:	
+	mov al, "-"
+	call WriteChar
+	loop Hor
+	call Crlf
+	pop eax
+	ret
+horizon_edge ENDP
+
+;-----------------------------------------
+default_draw PROC
+;-----------------------------------------
+	add esi, count
+    mov edx, [esi]
+    call WriteString
+	call vertical_edge
+	ret
+default_draw ENDP
+
+;-----------------------------------------
+color_draw PROC
+;-----------------------------------------
+	mov eax, white + (red * 16)
+	call setTextcolor
+	call default_draw
+	mov eax, white + (black * 16)
+	call setTextColor
+	ret
+color_draw ENDP
+
+;-----------------------------------------
+paint_block PROC
+;-----------------------------------------
+	.IF now_block == eax
+		call color_draw
+	.ELSE
+		call default_draw
+	.ENDIF
+	ret
+paint_block ENDP
+;-----------------------------------------
+play PROC
+;-----------------------------------------
+	mov ecx, random_num
+rotate:
+	mov dl,0
+	mov dh,0
+	call gotoxy ;刷新螢幕畫面
+
+	.IF now_block <= 0
+		add now_block, 19
+	.ELSE
+		dec now_block
+	.ENDIF
+
+	call Interface		;畫介面
+	
+	.IF ecx <= 5
+		push ecx
+		add time_interval, 200
+		Invoke sleep, time_interval
+		pop ecx
+	.ENDIF
+	loop rotate
+	ret
+play ENDP
+
+main PROC
+
+	call Randomize
+	mov eax, 20
+	call RandomRange
+	add eax, 10				;總共要轉幾格
+	mov random_num, eax
+	mov eax, 19
+	call RandomRange
+	inc eax
+	mov now_block, eax		;現在轉到的位置
+
+	call play
+	
+	Invoke ExitProcess, 0
+main ENDP
+end main
